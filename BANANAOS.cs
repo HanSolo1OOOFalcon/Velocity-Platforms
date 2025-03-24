2aa130fc82e945e4af7db540f4a13498c5d9a417
HanSolo1000Falcon <202703912+HanSolo1OOOFalcon@users.noreply.github.com>
2025-03-24 21:24:52 +0100

    Just the source code.

---
+++ BANANAOS.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using UnityEngine;
+using static VelocityPlatforms.Plugin;
+
+namespace BananaOS.Pages
+{
+    public class ExamplePage : WatchPage
+    {
+        public override string Title => "Velocity Platforms";
+        public override bool DisplayOnMainMenu => true;
+        public override void OnPostModSetup()
+        {
+            selectionHandler.maxIndex = 0;
+        }
+
+        //What you return is what is drawn to the watch screen the screen will be updated everytime you press a button
+        public override string OnGetScreenContent()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("<color=yellow>==</color> Velocity Platforms <color=yellow>==</color>");
+            stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(0, "Toggled"));
+            return stringBuilder.ToString();
+        }
+
+        public override void OnButtonPressed(WatchButtonType buttonType)
+        {
+            switch (buttonType)
+            {
+                case WatchButtonType.Up:
+                    selectionHandler.MoveSelectionUp();
+                    break;
+
+                case WatchButtonType.Down:
+                    selectionHandler.MoveSelectionDown();
+                    break;
+
+                case WatchButtonType.Enter:
+                    if (selectionHandler.currentIndex == 0)
+                    {
+                        enabled = !enabled;
+                        return;
+                    }
+                    break;
+
+                case WatchButtonType.Back:
+                    ReturnToMainMenu();
+                    break;
+            }
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace VelocityPlatforms
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.hansolo1000falcon.gorillatag.modtemplate";
+		public const string Name = "ModTemplate";
+		public const string Version = "1.0.0";
+	}
+}
