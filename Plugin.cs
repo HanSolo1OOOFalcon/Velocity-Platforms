b7b377129e6799827d25224b6ed177c5da7259e3
HanSolo1000Falcon <202703912+HanSolo1OOOFalcon@users.noreply.github.com>
2025-03-26 18:49:45 +0100

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -6,175 +6,191 @@ using Utilla;
 
 namespace VelocityPlatforms
 {
-  /// <summary>
-  /// This is your mod's main class.
-  /// </summary>
+	/// <summary>
+	/// This is your mod's main class.
+	/// </summary>
 
-  /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
-  [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
-  [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
-  public class Plugin : BaseUnityPlugin
-  {
-    void OnEnable()
+	/* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
     {
-      /* Set up your mod here */
-      /* Code here runs at the start and whenever your mod is enabled*/
+		void OnEnable()
+		{
+			/* Set up your mod here */
+			/* Code here runs at the start and whenever your mod is enabled*/
 
-      HarmonyPatches.ApplyHarmonyPatches();
-    }
+			HarmonyPatches.ApplyHarmonyPatches();
+		}
 
-    void OnDisable()
-    {
-      /* Undo mod setup here */
-      /* This provides support for toggling mods with ComputerInterface, please implement it :) */
-      /* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
+		void OnDisable()
+		{
+			/* Undo mod setup here */
+			/* This provides support for toggling mods with ComputerInterface, please implement it :) */
+			/* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
 
-      HarmonyPatches.RemoveHarmonyPatches();
-    }
+			HarmonyPatches.RemoveHarmonyPatches();
+		}
 
-    void OnGameInitialized(object sender, EventArgs e)
-    {
-	/* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
-    }
+		void OnGameInitialized(object sender, EventArgs e)
+		{
+			/* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
+		}
 
-    void Start()
-    {
-      GorillaTagger.OnPlayerSpawned(Init);
-    }
-
-    private void Init()
-    {
-      NetworkSystem.Instance.OnJoinedRoomEvent += OnJoinedRoom;
-      NetworkSystem.Instance.OnReturnedToSinglePlayer += OnLeftRoom;
-    }
-
-    private void OnJoinedRoom()
-    {
-      if (NetworkSystem.Instance.GameModeString.Contains("MODDED"))
-      {
-        EnsurePlatformsExist();
-      }
-      else
-      {
-        RemovePlatforms();
-      }
-    }
-    
-    private void OnLeftRoom()
-    {
-      RemovePlatforms();
-    }
-
-    private static GameObject lPlat = null;
-    private static GameObject rPlat = null;
-    private static GameObject CreatePlatform()
-    {
-      GameObject plat = GameObject.CreatePrimitive(PrimitiveType.Cube);
-      plat.transform.localScale = new Vector3(0.03f, 0.4f, 0.4f);
-      plat.GetComponent<Renderer>().material.shader = Shader.Find("GorillaTag/UberShader");
-      plat.GetComponent<Renderer>().material.color = Color.yellow;
-      plat.SetActive(false);
-      return plat;
-    }
-
-    private static void EnsurePlatformsExist()
-    {
-      if (lPlat == null)
-      {
-        lPlat = CreatePlatform();
-      }
-
-      if (rPlat == null)
-      {
-        rPlat = CreatePlatform();
-      }
-    }
-
-    private static void RemovePlatforms()
-    {
-      if (lPlat != null)
-      {
-        Destroy(lPlat);
-        lPlat = null;
-      }
-      
-      if (rPlat != null)
-      {
-        Destroy(rPlat);
-        rPlat = null;
-      }
-    }
-
-    private static Vector3 lastPositionL = Vector3.zero;
-    private static Vector3 lastPositionR = Vector3.zero;
-    private static Vector3 lastHeadPosition = Vector3.zero;
-
-    private static bool lHappen = false;
-    private static bool rHappen = false;
-    public static bool enabledPlugin = true;
-
-    private static float threshold = 0.05f;
-    void Update()
-    {
-      if (!NetworkSystem.Instance.InRoom)
-      {
-        RemovePlatforms();
-        return;
-      }
-      else if (!NetworkSystem.Instance.GameModeString.Contains("MODDED"))
-      {
-        RemovePlatforms();
-        return;
-      }
-
-      EnsurePlatformsExist();
-
-      threshold += GorillaLocomotion.GTPlayer.Instance.headCollider.transform.position.y - lastHeadPosition.y;
-
-      if (GorillaTagger.Instance.leftHandTransform.position.y + threshold <= lastPositionL.y && enabledPlugin)
-      {
-        if (!lHappen)
+        void Start()
         {
-          lHappen = true;
-
-          lPlat.transform.position = GorillaTagger.Instance.leftHandTransform.position;
-          lPlat.transform.rotation = GorillaTagger.Instance.leftHandTransform.rotation;
-          lPlat.SetActive(true);
+            GorillaTagger.OnPlayerSpawned(Init);
         }
-      }
-      else
-      {
-        if (lHappen)
+
+        private void Init()
         {
-          lHappen = false;
-          lPlat.SetActive(false);
+            NetworkSystem.Instance.OnJoinedRoomEvent += OnJoinedRoom;
+            NetworkSystem.Instance.OnReturnedToSinglePlayer += OnLeftRoom;
         }
-      }
 
-      if (GorillaTagger.Instance.rightHandTransform.position.y + threshold <= lastPositionR.y && enabledPlugin)
-      {
-        if (!rHappen)
+        private void OnJoinedRoom()
         {
-          rHappen = true;
-
-          rPlat.transform.position = GorillaTagger.Instance.rightHandTransform.position;
-          rPlat.transform.rotation = GorillaTagger.Instance.rightHandTransform.rotation;
-          rPlat.SetActive(true);
+            if (NetworkSystem.Instance.GameModeString.Contains("MODDED"))
+            {
+                EnsurePlatformsExist();
+            }
+            else
+            {
+                RemovePlatforms();
+            }
         }
-      }
-      else
-      {
-        if (rHappen)
+        private void OnLeftRoom()
         {
-          rHappen = false;
-          rPlat.SetActive(false);
+            RemovePlatforms();
         }
-      }
 
-      lastPositionL = GorillaTagger.Instance.leftHandTransform.position;
-      lastPositionR = GorillaTagger.Instance.rightHandTransform.position;
-      lastHeadPosition = GorillaLocomotion.GTPlayer.Instance.headCollider.transform.position;
-      threshold = 0.05f;
+		// The methods/functions running the platform logic.
+        private static GameObject lPlat = null;
+        private static GameObject rPlat = null;
+        private static GameObject CreatePlatform()
+		{
+			GameObject plat = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            		plat.transform.localScale = new Vector3(0.03f, 0.4f, 0.4f);
+			plat.GetComponent<Renderer>().material.shader = Shader.Find("GorillaTag/UberShader");
+            		plat.GetComponent<Renderer>().material.color = Color.yellow;
+			plat.SetActive(false);
+			return plat;
+        }
+
+		private static void EnsurePlatformsExist()
+		{
+			if (lPlat == null)
+			{
+				lPlat = CreatePlatform();
+            }
+
+            if (rPlat == null)
+			{
+                rPlat = CreatePlatform();
+            }
+        }
+
+		private static void RemovePlatforms()
+		{
+            if (lPlat != null)
+            {
+                Destroy(lPlat);
+                lPlat = null;
+            }
+            if (rPlat != null)
+            {
+                Destroy(rPlat);
+                rPlat = null;
+            }
+        }
+
+		// Initialize the last positions.
+        private static Vector3 lastPositionL = Vector3.zero;
+		private static Vector3 lastPositionR = Vector3.zero;
+        private static Vector3 lastPositionHead = Vector3.zero;
+
+        // The bools.
+        private static bool lHappen = false;
+        private static bool rHappen = false;
+        public static bool enabledPlugin = true;
+
+        // The main method.
+        private static float threshold = 0.05f;
+        void Update()
+		{
+            // Just the checks to make sure the room thingy majig is correct.
+            if (!NetworkSystem.Instance.InRoom)
+            {
+                RemovePlatforms();
+                return;
+            }
+            else if (!NetworkSystem.Instance.GameModeString.Contains("MODDED"))
+            {
+                RemovePlatforms();
+                return;
+            }
+
+            EnsurePlatformsExist();
+
+            // Calculate movement deltas
+            Vector3 headMovementDelta = GorillaTagger.Instance.headCollider.transform.position - lastPositionHead;
+            Vector3 leftHandMovementDelta = GorillaTagger.Instance.leftHandTransform.position - lastPositionL;
+            Vector3 rightHandMovementDelta = GorillaTagger.Instance.rightHandTransform.position - lastPositionR;
+
+            // Check if hand movements are similar to head movement
+            bool leftHandMovingWithHead = Vector3.Dot(headMovementDelta.normalized, leftHandMovementDelta.normalized) > 0.9f;
+            bool rightHandMovingWithHead = Vector3.Dot(headMovementDelta.normalized, rightHandMovementDelta.normalized) > 0.9f;
+
+            if (!leftHandMovingWithHead)
+            {
+                if (GorillaTagger.Instance.leftHandTransform.position.y + threshold <= lastPositionL.y && enabledPlugin)
+                {
+                    if (!lHappen)
+                    {
+                        lHappen = true;
+
+                        lPlat.transform.position = GorillaTagger.Instance.leftHandTransform.position;
+                        lPlat.transform.rotation = GorillaTagger.Instance.leftHandTransform.rotation;
+                        lPlat.SetActive(true);
+                    }
+                }
+                else
+                {
+                    if (lHappen)
+                    {
+                        lHappen = false;
+                        lPlat.SetActive(false);
+                    }
+                }
+            }
+
+            if (!rightHandMovingWithHead)
+            {
+                if (GorillaTagger.Instance.rightHandTransform.position.y + threshold <= lastPositionR.y && enabledPlugin)
+                {
+                    if (!rHappen)
+                    {
+                        rHappen = true;
+
+                        rPlat.transform.position = GorillaTagger.Instance.rightHandTransform.position;
+                        rPlat.transform.rotation = GorillaTagger.Instance.rightHandTransform.rotation;
+                        rPlat.SetActive(true);
+                    }
+                }
+                else
+                {
+                    if (rHappen)
+                    {
+                        rHappen = false;
+                        rPlat.SetActive(false);
+                    }
+                }
+            }
+
+            // Last positions of tracked objects.
+            lastPositionL = GorillaTagger.Instance.leftHandTransform.position;
+            lastPositionR = GorillaTagger.Instance.rightHandTransform.position;
+            lastPositionHead = GorillaTagger.Instance.headCollider.transform.position;
+        }
     }
-  }
 }
