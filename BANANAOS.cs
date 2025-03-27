b7fdf98a897d28c8cad5da3217d9e7fb8724de7d
HanSolo1000Falcon <202703912+HanSolo1OOOFalcon@users.noreply.github.com>
2025-03-27 09:03:00 +0100

    Update BANANAOS.cs

--- BANANAOS.cs
+++ BANANAOS.cs
@@ -4,10 +4,10 @@ using static VelocityPlatforms.Plugin;
 
 namespace BananaOS.Pages
 {
-    public class ExamplePage : WatchPage
+    public class BANANAOS : WatchPage
     {
         public override string Title => "Velocity Platforms";
-        public override bool DisplayOnMainMenu => true;
+        public override bool DisplayOnMainMenu => ciEnabled;
         public override void OnPostModSetup()
         {
             selectionHandler.maxIndex = 0;
@@ -17,7 +17,9 @@ namespace BananaOS.Pages
         {
             var stringBuilder = new StringBuilder();
             stringBuilder.AppendLine("<color=yellow>==</color> Velocity Platforms <color=yellow>==</color>");
-            stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(0, "Toggled"));
+            stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(0, "Toggled\n"));
+            stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(1, "Change right platform color."));
+            stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(2, "Change left platform color."));
             return stringBuilder.ToString();
         }
 
@@ -37,7 +39,17 @@ namespace BananaOS.Pages
                     if (selectionHandler.currentIndex == 0)
                     {
                         enabledPlugin = !enabledPlugin;
-                        BananaNotifications.DisplayNotification(enabledPlugin ? "<align=center><size=5> Enabled" : "<align=center><size=5> Disabled", Color.yellow, enabledPlugin ? Color.green : Color.red, 1f);
+                        BananaNotifications.DisplayNotification(enabledPlugin ? "<align=center><size=7> Enabled" : "<align=center><size=7> Disabled", Color.yellow, enabledPlugin ? Color.green : Color.red, 1f);
+                        return;
+                    }
+                    if (selectionHandler.currentIndex == 1)
+                    {
+                        colorIndexR++;
+                        return;
+                    }
+                    if (selectionHandler.currentIndex == 2)
+                    {
+                        colorIndexL++;
                         return;
                     }
                     break;
